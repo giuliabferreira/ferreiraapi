07d728040994d074317775cf228c069a0c8dfac6
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-09-27 09:50:10 -0300

    Update V001__Criar_Tabela_Cliente.sql

--- src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
+++ src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
@@ -3,3 +3,15 @@ id  BigInt not null primary key auto_increment,
 nome VARCHAR(150)
 );
 insert into Cliente(nome) values ('adalberto');
+insert into Cliente(nome) values ('jose');
+insert into Cliente(nome) values ('rosana');
+insert into Cliente(nome) values ('cleiton');
+insert into Cliente(nome) values ('roberta');
+
+Create table Contato (
+id  BigInt not null primary key auto_increment,
+descricao VARCHAR(300),
+contato VARCHAR
+);
+
+insert into Contato
