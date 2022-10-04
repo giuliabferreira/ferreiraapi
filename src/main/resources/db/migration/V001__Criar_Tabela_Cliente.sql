0784ee53764e36632cd42500cd648b6892dc9dcd
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-10-04 20:20:17 -0300

    criar tabela contato

--- src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
+++ src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
@@ -8,10 +8,5 @@ insert into Cliente(nome) values ('rosana');
 insert into Cliente(nome) values ('cleiton');
 insert into Cliente(nome) values ('roberta');
 
-Create table Contato (
-id  BigInt not null primary key auto_increment,
-descricao VARCHAR(300),
-contato VARCHAR
-);
 
-insert into Contato
+
---
+++ src/main/resources/db/migration/V002__Criar_Tabela_Contato.sql
@@ -0,0 +1,11 @@
+Create table Contato (
+id  BigInt not null primary key auto_increment,
+descricao VARCHAR(300),
+contato VARCHAR(300)
+);
+
+insert into Contato(contato) values ('gabrielHsantos2018@gmail.com');
+insert into Contato(contato) values ('gabrielHsantos2019@gmail.com');
+insert into Contato(contato) values ('gabrielHsantos2020@gmail.com');
+insert into Contato(contato) values ('gabrielHsantos2021@gmail.com');
+insert into Contato(contato) values ('gabrielHsantos2022@gmail.com');
