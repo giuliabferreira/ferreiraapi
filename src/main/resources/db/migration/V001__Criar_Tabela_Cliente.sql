e0e1317235c977cdaa2ecf5e9519b676f68e90d4
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-10-04 21:26:10 -0300

    chaves estrangeiras

--- src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
+++ src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
@@ -1,12 +1,14 @@
 Create table Cliente(
 id  BigInt not null primary key auto_increment,
-nome VARCHAR(150)
+nome VARCHAR(150),
+contatoid bigint not null
 );
-insert into Cliente(nome) values ('adalberto');
-insert into Cliente(nome) values ('jose');
-insert into Cliente(nome) values ('rosana');
-insert into Cliente(nome) values ('cleiton');
-insert into Cliente(nome) values ('roberta');
+Alter table CLiente add constraint FK_cliente_contato foreign key(contatoid) references contato(id);
+insert into Cliente(nome,contatoid) values ('adalberto',1);
+insert into Cliente(nome,contatoid) values ('jose',2);
+insert into Cliente(nome,contatoid) values ('rosana',3);
+insert into Cliente(nome,contatoid) values ('cleiton',4);
+insert into Cliente(nome,contatoid) values ('roberta',5);
 
 
 
--- src/main/resources/db/migration/V002__Criar_Tabela_Contato.sql
+++ src/main/resources/db/migration/V002__Criar_Tabela_Contato.sql
@@ -1,11 +1,12 @@
-Create table Contato (
+Create table Contato(
 id  BigInt not null primary key auto_increment,
 descricao VARCHAR(300),
-contato VARCHAR(300)
+contato VARCHAR(300),
+clienteid bigint not null
 );
-
-insert into Contato(contato) values ('gabrielHsantos2018@gmail.com');
-insert into Contato(contato) values ('gabrielHsantos2019@gmail.com');
-insert into Contato(contato) values ('gabrielHsantos2020@gmail.com');
-insert into Contato(contato) values ('gabrielHsantos2021@gmail.com');
-insert into Contato(contato) values ('gabrielHsantos2022@gmail.com');
+Alter table Contato add constraint FK_contato_cliente foreign key(clienteid) references cliente(id);
+insert into Contato(contato,clienteid) values ('gabrielHsantos2018@gmail.com',1);
+insert into Contato(contato,clienteid) values ('gabrielHsantos2019@gmail.com',2);
+insert into Contato(contato,clienteid) values ('gabrielHsantos2020@gmail.com',3);
+insert into Contato(contato,clienteid) values ('gabrielHsantos2021@gmail.com',4);
+insert into Contato(contato,clienteid) values ('gabrielHsantos2022@gmail.com',5);
