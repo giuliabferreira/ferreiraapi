3634612dca707c5c1aef7a40c5bb0dea48181998
Gabriel Henrique dos Santos <gabrielHsantos2018@gmail.com>
2022-10-05 11:24:02 -0300

    correçao

--- src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
+++ src/main/resources/db/migration/V001__Criar_Tabela_Cliente.sql
@@ -3,7 +3,6 @@ id  BigInt not null primary key auto_increment,
 nome VARCHAR(150),
 contatoid bigint not null
 );
-Alter table CLiente add constraint FK_cliente_contato foreign key(contatoid) references contato(id);
 insert into Cliente(nome,contatoid) values ('adalberto',1);
 insert into Cliente(nome,contatoid) values ('jose',2);
 insert into Cliente(nome,contatoid) values ('rosana',3);
